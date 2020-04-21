18fb114b0d293d38c771b4f45118e0e39cb79506
soycoder <43911669+soycoder@users.noreply.github.com>
2020-04-21 17:34:46 +0700

    new Auth TU v2

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const https = require("https");
+// defining the Express app
+const app = express();
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan("combined"));
+
+// defining an endpoint to return all ads
+app.get("/api/auth/", (_req, _res) => {
+  var data;
+  var options = {
+    method: "POST",
+    hostname: "restapi.tu.ac.th",
+    path: "/api/v1/auth/Ad/verify",
+    headers: {
+      "Content-Type": "application/json",
+      "Application-Key":
+        "MjU0OTNkYjM1MWE5Mzk0MGVlNzU3MGUyMzRiYWQ0N2ZkOGFmMGJkMWVkNjBiMDEwYTJhZTliMzNkZGU5ZTMzMw==",
+    },
+  };
+
+  var req = https.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function (chunk) {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+      _res.status(200).json(body.toString());
+    });
+
+    res.on("error", function (error) {
+      console.error(error);
+    });
+  });
+
+  var postData =
+    '{\n\t"UserName":"' +
+    _req.body.username +
+    '",\n\t"PassWord":"' +
+    _req.body.password +
+    '"\n}';
+
+  req.write(postData);
+
+  req.end();
+});
+
+// starting the server
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
